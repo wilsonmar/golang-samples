540906ce949c1fc78d2b3ead843c8116efbbf847
Wilson Mar <wilsonmar@gmail.com>
2017-04-15 18:44:05 -0600

    Update packages to install

--- hello/hello.go
+++ hello/hello.go
@@ -1,8 +1,10 @@
-// hello.go
+/* hello.go from https://github.com/wilsonmar/golang-sample
+   Sample output: "hello, world at 2017-04-15 17:32:52.470348041 -0600 MDT
+*/
 package main
-import "fmt"
-import "time"
+import("fmt"; "time") // notice semicolons are used. see https://golang.org/pkg/time/
 var now = time.Now()
 func main() {
-    fmt.Printf("hello, world at %v",now)
+    fmt.Printf("hello, world at %v.",now)
+    fmt.Printf("\n")
 }
