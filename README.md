923347bab5ab468d494858ebaab9ef5a8d2ba233
Wilson Mar <wilsonmar@gmail.com>
2017-04-16 06:50:07 -0600

    hello-time from hello

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# golang
-Sample code
+This repository contains simple sample code in the Go language.
+
+1. It would be an honor for me if you click the Star icon.
+
+1. Fork this to your account because I assume you'll want to make changes.
+
+   ### On your local machine:
+
+1. Clone your version of the repo onto your local machine.
+
+1. Install the Go compiler (if you haven't already).
+
+   ### On a Terminal Shell window:
+
+1. Define a global shell variable to your cloned repo.
+
+1. cd into a program's folder.
+
+   ### Hello program
+
+   The <strong>hello</strong> program contains an enhanced "hello world" example.
+
+1. For testing, build the source into an executable and invoke it in one step:
+
+   <pre>
+   cd hello
+   go run hello.go
+   cd ..
+   </pre>
+
+1. Build the source files into a new executable file (with no file extension), then run the executable:
+
+   <pre>
+   cd hello
+   go build 
+   ./hello
+   cd ..
+   </pre>
+
+1. Execute the executable created:
+
+   <pre>
+   chmod 555 hello
+   ./hello
+   cd ..
+   </pre>
+
+## Others
+
+* https://github.com/mkaz/working-with-go
---
+++ hello-time/hello-time.go
@@ -0,0 +1,106 @@
+/* hello-time.go from https://github.com/wilsonmar/golang-sample
+   A tutorial for first-time developers of GoLang.
+*/
+
+// All Go programs must be part of a package.
+// main is the default package.
+package main
+
+// import libraries:
+// fmt provides basic standard out formatting functions.
+import("fmt"; "time"; "strconv") // notice semicolons are used. see https://golang.org/pkg/time/
+//import("fmt"; "time"; "strings") // notice semicolons are used. see https://golang.org/pkg/time/
+
+const (
+    // For use with http://golang.org/pkg/time/#Parse
+    timeLayout = "2006-01-02 15:04 MST"
+)
+
+// The one of a few reasons for a global variable:
+var runtime_start  = time.Now() // See: http://golang.org/pkg/time/
+
+// A function must match the package name to
+// provide an entry point which gets called when run:
+func main() {
+    // Four character indent.
+
+  	// Go requires a time zone when specifying a date, so build one:
+  	milestone_tz, _ := time.LoadLocation("MST") // You can also use time.UTC constant
+  	milestone_date := time.Date(2009, 11, 10, 23, 15, 4, 5, milestone_tz)
+    // Format by example does not recognize use of %v
+    fmt.Println("Go became an open source project on",milestone_date.Format("Mon, Jan 2, 2006 at 3:04pm"))
+    // No space needs to be added in the text above.
+    // fmt.Printf("\n")  // blank line
+
+    // Code in the Go Playground http://play.golang.org/p/m5AWasfhQy
+    // always returns timestamps at 1257894000 (Tue, 10 Nov 2009 23:00:00),
+    // the date Go became a public open source project.
+
+    now := time.Now()
+    epoch_start := now.Unix()  // Alternate way to define variable
+
+  	// BLAH: Duration objections do not have days due to timezones
+  	// and daylight savings time (which we think is a cop out but whatever)
+    // Time Addition/Subtraction returns a Duration object
+  	diff := now.Sub(milestone_date)
+  	diff_days := int(diff.Hours() / 24)
+  	fmt.Printf("That's %d days ago\n", diff_days)
+
+
+    // The Println method from fmt package writes to standard out:
+    fmt.Printf("from now at %v (default format)",now)
+    fmt.Printf("\n") // Blank line needs to be specified.
+
+    // Use built-in time constants for date formatting
+  	// See Time constans for more: http://golang.org/pkg/time/#pkg-constants
+  	fmt.Println("RFC850:  ",now.Format(time.RFC850))
+  	fmt.Println("RFC1123:  ",now.Format(time.RFC1123))
+
+    // Double quotes within double quotes:
+    fmt.Printf("That's %v seconds from midnight, January 1, 1970 (\"epoch time\")",epoch_start)
+    // PROTIP: Put blank lines in a separate line for easier refactoring:
+    fmt.Printf("\n")
+
+
+    // Construct date of next Christmas:
+    // See https://stackoverflow.com/questions/1760757/how-to-efficiently-concatenate-strings-in-go
+  	// Parsing user inputted dates:
+    //   timeLayout = "2006-01-02 15:04 MST"
+    // Construct a date string without error:
+    christmas_text := strconv.Itoa(now.Year()) + "-12-25 00:00:00" // 0:0:0 won't dd.
+    //christmas_text := "2017-12-26 12:00 MST" // + milestone_tz
+  	// layout := "2/30/2014 11:38am" // underline is "magic" variable compiler ignores
+  	then, err := time.Parse("2006-01-02 15:04:05", christmas_text)
+  	if err != nil {
+  		 fmt.Println(">>> ",err)
+       // return  // due to err
+  	}
+    // Calculate difference in dates from now, made positive:
+    diff_now := time.Since(then) * -1
+    // BLAH: Duration objections do not have days due to timezones and daylight savings time.
+    // Time Addition/Subtraction returns a Duration object
+    diff_xmas := int(diff_now.Hours() / 24)
+  	fmt.Println("Add",diff_xmas,"days until next Midnight Christmas on", then.Format("Monday, Jan 2, 2006"))
+    // See https://golang.org/pkg/fmt/
+
+    /*
+  	twodaysDiff := time.Hour * 24 * 2
+  	twodays := now.Add(twodaysDiff)
+  	fmt.Println("Two Days: ", twodays.Format(time.ANSIC))
+
+    // Time Conditionals: Use Before() After() Equal() to compare dates:
+  	if milestone_date.Before(now) {
+  		fmt.Println("That is before Now ")
+  	}
+    */
+
+
+    runtime_elapsed := time.Since(runtime_start)
+    fmt.Printf("Execution time: %s microseconds", runtime_elapsed)
+    // See https://coderwall.com/p/cp5fya/measuring-execution-time-in-go
+
+    fmt.Printf("\n") // added so new prompt begins from left margin.
+}
+// Sample output containing the default time stamp format:
+// hello, world at 2017-04-16 04:15:35.824976829 -0600 MDT.
+// EOF
