f7f5c5ada0403ae19bd06e57e339f0119c1ff24e
Wilson Mar <wilsonmar@gmail.com>
2017-04-17 19:00:45 -0500

    latlong float compare

--- latlong.go
+++ latlong.go
@@ -1,14 +1,17 @@
 /* latlong.go
    Manages latitudes and longitudes in a map data type containing a key-value structure.
    Uses Go map functionality.
-   Uses float64
+	 Calculates a absolute value in a rough comparison of two floats using Newton's method with a tolerance.
    Uses function to do repeated lookups
    Adapted from https://tour.golang.org/moretypes/19
 */
 
 package main
 
-import "fmt"
+import(
+	"fmt"
+	"math"  // http://golang.org/pkg/math/
+)
 
 type Vertex struct {
 	Lat, Long float64 // Degrees and decimal minutes (DMM)
@@ -24,15 +27,31 @@ func main() {
 	m["Machu Pichu"] = Vertex{ -13.1631, -72.5450 } // 13.1631° S, 72.5450° W (negative/left of UTC)
 	m["Taj Mahal"]   = Vertex{  27.1750, 78.0422 } // North and East of UTC
     // TODO: Add more values.
-    // TODO: Read from a file.
+    // TODO: Read vertexes from a file.
 
 	// Lookup by key:
-    // key := "Bell Labs"
   key := "Taj Mahal"
 	fmt.Println(key,"is at Longitude,Latitude",m[key])
 	//LatLong("Bell Labs")
 	//LatLong("Taj Mahal")
 	//LatLong("Machu Pichu")
+
+
+
+  // Determine if two floats are roughly equal within a tolerance:
+	const TOLERANCE = 0.000001
+	result := 27.1750
+	temp   := 27.17501
+	for { // Using Newton's method - see https://coderwall.com/p/pzhz9q/comparing-floating-point-integers-in-golang
+	  if diff := math.Abs(result - temp); diff < TOLERANCE { // consider equal and break
+			fmt.Println("Within tolerance of",TOLERANCE)
+	    break
+	  } else {
+			// Loop here to do something:
+			fmt.Println(result,"and",temp,"not the same.")
+			break
+	  }
+	}
 }
 
 // See https://github.com/mkaz/working-with-go/blob/master/04-functions.go
--- rand.go
+++ rand.go
@@ -1,6 +1,8 @@
 /* rand.go at https://github.com/wilsonmar/golang-samples/rand
+   Option to output a flat range as an integer or a floating point number.
+
    Adapted from https://www.golang-book.com/books/intro/13#section8 for command line arguments
-   And https://www.golang-book.com/books/intro/13#section6 which doesn't have a randome seed.
+   And https://www.golang-book.com/books/intro/13#section6 which doesn't have a random seed.
    And https://stackoverflow.com/questions/12321133/golang-random-number-generator-how-to-seed-properly
 */
 
@@ -10,16 +12,17 @@ import (
   "fmt"
   "time" // for random seed generation
   "flag" // to obtain arguments from command line
-  "math/rand"
+  "math/rand"  // http://golang.org/pkg/math/rand/
 )
 
 func main() {
 
   // Define flags (command line arguments):
-  max_p := flag.Int("max", 100, "the max value") // Define default 100 if -max=? is not specified in the run command.
-  //repeats := flag.Int("repeat", 1, "the number of outputs")
+  max_p   := flag.Int("max", 100, "the max value") // Define default 100 if -max=? is not specified in the run command.
+  repeats := flag.Int("repeat", 1, "the number of outputs")
+  distribution := flag.Int("distribution", 1, "the type of distribution")
 
-  // So each run returns a different number, get a random seed based on the time :
+  // So each run returns a different number, get a (deterministic) random seed based on a precise time :
   rand.Seed(time.Now().UTC().UnixNano()) // value is a epoch date/time stamp like 1492421032295856507
 
   // Parse flag (command line arguments) from the go run command:
@@ -31,11 +34,33 @@ func main() {
   // TODO: Send to a file.
 
   // TODO: Loop the number of repeats specified:
-  //for i := 0; i < repeats; i++ {
+  // PROTIP: Reference argument flag integers as pointers:
+  fmt.Println("Repeats flag:", *repeats)
+  fmt.Println("Distribution flag:", *distribution)
+
+  for i := 0; i < *repeats; i++ {
       // BLAH: ./rand.go:33: invalid operation: i < repeats (mismatched types int and *int)
-      // Intn gets the next random integer. Then Print the contents of pointer to the flag variable:
-      fmt.Println(rand.Intn(*max_p))
-  //}
+
+      // There are several types of random numbers:
+
+      // distribution == 1 (default)
+
+         // Intn (integer number) gets the next random integer. Then Print the contents of pointer to the flag variable:
+         fmt.Println("Of", *max_p, ":", rand.Intn(*max_p))
+
+         // Flat chance:
+      	 fmt.Println("Random Int:", rand.Int())
+
+      	 fmt.Println("Random Float:", rand.Float32())
+
+      // distribution == 2
+       	 // A not so random function in the math/rand package
+       	 // The NormFloat64 method returns a number based on Normal distrib
+        	// mean=0, stddev=1 -- if called enough and plotted will give bell curve
+        fmt.Println("Normalized:", rand.NormFloat64())
+
+        // fmt.Println("Random Permutation of N ints:", rand.Perm(8))
+  }
 }
 /* Respoonse to go run sha1.go -max=50
 31
